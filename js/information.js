7d6bf611d2b083b269253f5aa174b4fb2d2ced52
kumankoishi.py <7iggif@naver.com>
2021-02-28 00:05:20 +0900

    Added checkPasswordsMatch()

--- js/information.js
+++ js/information.js
@@ -4,10 +4,24 @@ function renderInformation () {
   `
 }
 
-function cryptPassword () {
+function checkPasswordsMatch () {
+  const query = 'input[type="password"]'
+  const passwords = document.querySelectorAll(query)
+  let allSame = true
+  for (let i in passwords) {
+    if (passwords[i] !== passwords[0]) {
+      allSame = false
+      break
+    }
+  }
+  return allSame
+}
 
+function cryptPassword () {
+  
 }
 
 function onSubmitSignUp () {
-  
+  console.log('onSubmitSignUp')
+  return false
 }
